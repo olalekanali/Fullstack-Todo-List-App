e8cc92b90d4c4bf77b59061e36e432e215e14711
Ali <harleyouharley@gmail.com>
2025-06-01 15:12:24 +0100

    modified

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -8,26 +8,26 @@ function App() {
 
   useEffect(() => {
     // Fetch from backend
-    axios.get(`${import.meta.env.BACKEND_URL}/api/todos`)
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/todos`)
       .then(res => setTodos(res.data))
       .catch(err => console.error(err));
   }, []);
 
   const addTodo = (text) => {
-    axios.post(`${import.meta.env.BACKEND_URL}/api/todos`, { text })
+    axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/todos`, { text })
       .then(res => setTodos([...todos, res.data]))
       .catch(err => console.error(err));
   };
 
   const toggleTodo = (id) => {
     const todo = todos.find(t => t._id === id);
-    axios.put(`${import.meta.env.BACKEND_URL}/api/todos/${id}`, { completed: !todo.completed })
+    axios.put(`${import.meta.env.VITE_BACKEND_URL}/api/todos/${id}`, { completed: !todo.completed })
       .then(res => setTodos(todos.map(t => t._id === id ? res.data : t)))
       .catch(err => console.error(err));
   };
 
   const deleteTodo = (id) => {
-    axios.delete(`${import.meta.env.BACKEND_URL}/api/todos/${id}`)
+    axios.delete(`${import.meta.env.VITE_BACKEND_URL}/api/todos/${id}`)
       .then(() => setTodos(todos.filter(t => t._id !== id)))
       .catch(err => console.error(err));
   };
